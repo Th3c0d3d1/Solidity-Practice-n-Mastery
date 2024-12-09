7e8f7b267f3d604f902801fd2cf18a2f97dadf1e
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-12-09 16:17:09 -0500

    Arrays Comments n testing

--- contracts/Arrays.sol
+++ contracts/Arrays.sol
@@ -4,32 +4,52 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 // Array initalization
-// Homework: try more data types!
+// Collection of items that can be sorted
+// Arrays can be of fixed size or dynamic
 contract Arrays1 {
+
+    // Initializes an array of uints
+    // Dynamic array (can grow/shrink in size)
+    // Can only be uint data type
     uint[] public array1 = [1, 2, 3];
-    uint[] public array2; // this initializes an array of uints
-    uint[10] public array3; // this initializes a fixed size array of 10 uints, default to 0
+
+    // Initializes an array of uints
+    uint[] public array2;
+
+    // Initializes a fixed size array of 10 uints, default to 0
+    // Fixed size array (cannot grow/shrink in size)
+    // Default array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    uint[10] public array3;
+
+    // Initializes an array of strings
+    // Dynamic array (can grow/shrink in size)
+    // Can only be string data type
     string[] public array4  = ["apple", "banana", "carrot"];
-    string[] public array5; // this initializes an array of strings
-    string[10] public array6; // this initializes a fixed size array of 10 uints, default to ""
+
+    // Initializes an array of strings
+    string[] public array5;
+
+    // Initializes a fixed size array of 10 uints, default to ""
+    // Default array = ["", "", "", "", "", "", "", "", "", ""]
+    string[10] public array6;
 }
 
-
 // Array functions
-// Homework: try manipulating more arrays & data types
 contract Arrays2 {
     uint[] public array;
 
-    // Get value at a given index
+    // Get/Fetch value at a given index
     function get(uint i) public view returns (uint) {
         return array[i];
     }
 
-    // Return the whole array
+    // Return the whole array from state variable
     function getArray() public view returns (uint[] memory) {
         return array;
     }
 
+    // Return the whole array from local variable
+    // Returns the length of the array
     function length() public view returns (uint) {
         return array.length;
     }
@@ -45,6 +65,7 @@ contract Arrays2 {
     }
 
     // Remove item at given index
+    // Doesn't remove the value at index, but sets it to 0/Default value
     function remove(uint index) public {
         delete array[index];
     }
