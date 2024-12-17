e40b851bc3111ef5019e8719bbb5e80818a2d9a4
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-12-17 16:29:19 -0500

    Mapping comments & testing

--- contracts/Mappings.sol
+++ contracts/Mappings.sol
@@ -4,23 +4,40 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 // Mapping examples (key value pairs)
+// Key can only have one value
 contract Mappings1 {
-    mapping(uint => string) public names; // Mapping of string like a databse
-    mapping(uint => address) public addresses; // Mapping of string like a databse
-    mapping(address => uint) public balances; // Mapping of string like a databse
-    mapping(address => bool) public hasVoted; // Tracks whether a user has voted
+
+    // Mapping of string like a database
+    // Uint is the key, string is the value
+    mapping(uint => string) public names;
+
+    // Mapping of addresses(string) like a database
+    mapping(uint => address) public addresses;
+
+    // Map balances to an address
+    mapping(address => uint) public balances;
+
+    // Tracks if a user has voted
+    mapping(address => bool) public hasVoted;
 
     // Add values to the mapping in the constructor
+    // This is a constructor function that runs when the contract is deployed
+    // Mappings can only be state variables
     constructor() {
+
+        // Assign values to the names mappings
         names[1] = "Adam";
         names[2] = "Ben";
 
+        // Assign values to the addresses mappings
         addresses[1] = 0x3EcEf08D0e2DaD803847E052249bb4F8bFf2D5bB;
         addresses[2] = 0xe5c430b2Dd2150a20f25C7fEde9981f767A48A3c;
 
+        // Assign values to the balances mappings
         balances[0x3EcEf08D0e2DaD803847E052249bb4F8bFf2D5bB] = 1 ether;
         balances[0xe5c430b2Dd2150a20f25C7fEde9981f767A48A3c] = 2 ether;
 
+        // Assign values to the hasVoted mappings
         hasVoted[0x3EcEf08D0e2DaD803847E052249bb4F8bFf2D5bB] = true;
         hasVoted[0xe5c430b2Dd2150a20f25C7fEde9981f767A48A3c] = true;
     }
@@ -38,39 +55,45 @@ contract Mappings2 {
     mapping(uint => Book) public books;
 
     // Nested mapping
-    // Track token balnces for a given account
-    // E.G. Dai: 0x6B175474E89094C44Da98b954EedeAC495271d0F
-    // E.G. WETH: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
+    // Track token balances for a given account
     mapping(address => mapping(address => uint)) public balances;
 
     constructor() {
+        // Assign values to the books mappings (Enter new books)
         books[1] = Book("A Tale of Two Cities", "Charles Dickens");
         books[2] = Book("Les Miserables", "Victor Hugo");
 
+        // Assign values to the balances mappings
         address user1 = 0x3EcEf08D0e2DaD803847E052249bb4F8bFf2D5bB;
         address user2 = 0xe5c430b2Dd2150a20f25C7fEde9981f767A48A3c;
+
+        // Token addresses assigned to address
+        // Assigned token addresses to check balances associated with the address
         address dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
         address weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
-        // User 1 dai balance
+        // Setting user balances
         balances[user1][dai] = 1 ether;
         balances[user2][weth] = 2 ether;
     }
 
 }
 
-// Getting and setting values
+// Getting and setting mapping values
 contract Mappings3 {
     mapping(uint => string) public myMapping;
 
+    // Get the value of a key
     function get(uint _id) public view returns (string memory) {
         return myMapping[_id];
     }
 
+    // Set the value of a key
     function set(uint _id, string memory _value) public {
         myMapping[_id] = _value;
     }
 
+    // Remove the value of a key
     function remove(uint _id) public {
         // Resets the value to the default value.
         delete myMapping[_id];
@@ -80,16 +103,21 @@ contract Mappings3 {
 
 // Getting and setting nested values
 contract Mappings4 {
+
+    // Nested mapping of possible existence of a key 
     mapping(address => mapping(uint => bool)) public myMapping;
 
+    // Get the value of a key
     function get(address _user, uint _id) public view returns (bool) {
         return myMapping[_user][_id];
     }
 
+    // Set the value of a key
     function set(address _user, uint _id, bool _value) public {
-       myMapping[_user][_id] = _value;
+        myMapping[_user][_id] = _value;
     }
 
+    // Remove the value of a key
     function remove(address _user, uint _id) public {
         // Resets the value to the default value.
         delete myMapping[_user][_id];
--- test/Mappings.js
+++ test/Mappings.js
@@ -14,14 +14,26 @@ describe('Mappings', () => {
     it('demonstrates basic mappings with default values', async () => {
       const Contract = await ethers.getContractFactory('Mappings1')
       let contract = await Contract.deploy()
+
+      // Check assigned name values
       expect(await contract.names(1)).to.equal('Adam')
       expect(await contract.names(2)).to.equal('Ben')
+
+      // Check unassigned/default name value
       expect(await contract.names(3)).to.equal('')
+
+      // Check assigned address values
       expect(await contract.addresses(1)).to.equal('0x3EcEf08D0e2DaD803847E052249bb4F8bFf2D5bB')
       expect(await contract.addresses(2)).to.equal('0xe5c430b2Dd2150a20f25C7fEde9981f767A48A3c')
+
+      // Check unassigned address value
       expect(await contract.addresses(3)).to.equal('0x0000000000000000000000000000000000000000')
+
+      // Check assigned bool values
       expect(await contract.hasVoted('0x3EcEf08D0e2DaD803847E052249bb4F8bFf2D5bB')).to.equal(true)
       expect(await contract.hasVoted('0xe5c430b2Dd2150a20f25C7fEde9981f767A48A3c')).to.equal(true)
+
+      // Check unassigned bool value
       expect(await contract.hasVoted('0x4f7d078Ed1A55a788e1e7eCD02f2c8249e2d11Ab')).to.equal(false)
     })
   })
@@ -35,7 +47,9 @@ describe('Mappings', () => {
       expect(result[0]).to.equal('A Tale of Two Cities')
       expect(result[1]).to.equal('Charles Dickens')
 
-      // Homework: check book 2
+      let result2 =  await contract.books(2)
+      expect(result2[0]).to.equal('Les Miserables')
+      expect(result2[1]).to.equal('Victor Hugo')
 
       let user1 = '0x3EcEf08D0e2DaD803847E052249bb4F8bFf2D5bB'
       let dai = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
@@ -83,9 +97,6 @@ describe('Mappings', () => {
 
       await contract.remove(user1, 1)
       expect(await contract.get(user1, 1)).to.equal(false)
-
     })
   })
-
-
 })
