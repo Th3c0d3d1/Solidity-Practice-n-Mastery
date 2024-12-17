4ecb03cceac47ee74f6d981a92c56bf0899feaea
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-12-17 17:43:49 -0500

    Events comments & testing

--- contracts/Events.sol
+++ contracts/Events.sol
@@ -3,14 +3,18 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-// Structs
+// Events are a way to log data on the blockchain
 contract Events1 {
     string public message = "Hello World";
 
+    // Define an event
     // Note: events can have up to 17 arguments
     // You can use any basic data type you want (e.g. string, uint, address, bool, etc...)
-    // You can index up to 3 arguments
     event MessageUpdated(
+
+        // The indexed keyword allows you to search for events based on the indexed argument
+        // Allows for subscription to an event based on a specific indexed argument(user in this case)
+        // You can index up to 3 arguments
         address indexed _user,
         string _message
     );
--- test/Events.js
+++ test/Events.js
@@ -16,28 +16,33 @@ describe('Events', () => {
       const Contract = await ethers.getContractFactory('Events1')
       let contract = await Contract.deploy()
 
-      // Call it once, check the event log in real time
+      // Pass value 'Hey!' into updateMessage function
+      // Call the tx once & check the event log in real time
       let transaction = await contract.updateMessage('Hey!')
       await transaction.wait()
       await expect(transaction).to.emit(contract, 'MessageUpdated')
         .withArgs(user1.address, 'Hey!')
 
-      // Call it a few more times to get event history
+      // Calling it a few more times to get event history
       transaction = await contract.updateMessage('Ho!')
       await transaction.wait()
 
       transaction = await contract.updateMessage('Ha!')
       await transaction.wait()
 
-      // Get all past events
+      // Getting all past events (stream)
       // https://docs.ethers.io/v5/getting-started/#getting-started--history
       let eventStream = await contract.queryFilter('MessageUpdated')
+      // Uncomment below to see the event stream
+      // console.log(eventStream)
       expect(eventStream.length).to.equal(3)
 
       // Check first event in the stream
-      // Homework: check other values and other events
       let firstEvent = eventStream[0]
+      // Uncomment below to see the event stream
+      // console.log(firstEvent)
       expect(firstEvent.args[1]).to.equal('Hey!')
+      expect(firstEvent.args[0]).to.equal(user1.address)
 
 
       // Trigger event from user 2
@@ -45,16 +50,18 @@ describe('Events', () => {
       await transaction.wait()
 
       // Filter only events created by user2
+      // Unindexed parameters can't be filtered (e.g. message argument = null )
+      // Indexed parameters can also left unfiltered by passing null
       let user2Filter = contract.filters.MessageUpdated(user2.address, null)
+
+      // Get all events from created user 2 filtered stream
       eventStream = await contract.queryFilter(user2Filter)
       expect(eventStream.length).to.equal(1)
 
-      // Make sure it's user 2's message
-      // Homework: check other parameters
+      // Make sure it's user 2's message & address
       firstEvent = eventStream[0]
       expect(firstEvent.args[1]).to.equal('Huh!')
-
+      expect(firstEvent.args[0]).to.equal(user2.address)
     })
   })
-
 })
