7fa7a7d1fd71fa7883b091f2d769ae448339b0d8
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-12-30 16:27:00 -0500

    Interactions update comments

--- contracts/Interactions.sol
+++ contracts/Interactions.sol
@@ -32,18 +32,22 @@ contract SecretVault {
 contract Interactions1 {
 
     // The contract SecretVault is imported as a state variable
+    // Refering to the SecretVault contract as public allows the contract to access the functions of the SecretVault contract
     SecretVault public secretVault;
 
     // The constructor takes an instance of the SecretVault contract and assigns it to the state variable secretVault
+    // Saves the address of the SecretVault contract
     constructor(SecretVault _secretVault) {
         secretVault = _secretVault;
     }
 
+    // Calling the setSecret function of the SecretVault contract
     // This function allows the contract to set the secret in the SecretVault contract
     function setSecret(string memory _secret) public {
         secretVault.setSecret(_secret);
     }
 
+    // Calling the getSecret function of the SecretVault contract
     // This function allows the contract to get the secret from the SecretVault contract
     function getSecret() public view returns(string memory) {
         return secretVault.getSecret();
@@ -52,9 +56,13 @@ contract Interactions1 {
 
 import "./Token.sol";
 
+// Interface for the ERC20 token
+// Using only the parts of the contract that are needed
+// This is a way to interact with the token contract without importing the whole contract
 interface IERC20 {
 
     // Returns the total token supply
+    // Does not need the function body
     function transferFrom(
         address _from,
         address _to,
@@ -68,7 +76,12 @@ contract Interactions2 {
 
     // This function allows the contract to deposit tokens into the contract
     function deposit(address _tokenAddress, uint _amount) public {
+
         // Transfer the tokens from the sender to the contract
+        // Talk to contract directly using the interface (IERC20) & pass the token address (_tokenAddress)
+        // Call the transferFrom function of the token contract
+        // No need to store the token contract in a state variable
+        // Instatiate the token contract using the token address
         IERC20(_tokenAddress).transferFrom(msg.sender, address(this), _amount);
     }
 }
