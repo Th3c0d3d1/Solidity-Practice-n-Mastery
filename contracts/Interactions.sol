e4ce230bac4deae6e5252166b94c9ce310c27ac6
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-12-30 15:54:32 -0500

    Interactions init comment & test

--- contracts/Interactions.sol
+++ contracts/Interactions.sol
@@ -6,24 +6,35 @@ import "hardhat/console.sol";
 // https://docs.soliditylang.org/en/v0.8.16/contracts.html?highlight=inheritance#inheritance
 
 contract SecretVault {
+
+    // State variables
     string private secret;
 
     constructor(string memory _secret) {
         secret = _secret;
     }
 
+    // Set the secret
+    // This function is only accessible by the contract itself
+    // Setters are functions that allow you to modify the state of a contract
     function setSecret(string memory _secret) external {
         secret = _secret;
     }
 
+    // Get the secret
+    // This function is accessible by anyone
+    // Getters are functions that allow you to read the state of a contract
     function getSecret() external view returns(string memory) {
         return secret;
     }
 }
 
 contract Interactions1 {
+
+    // The contract SecretVault is imported as a state variable
     SecretVault public secretVault;
 
+    // The constructor takes an instance of the SecretVault contract and assigns it to the state variable secretVault
     constructor(SecretVault _secretVault) {
         secretVault = _secretVault;
     }
--- test/Interactions.js
+++ test/Interactions.js
@@ -12,38 +12,46 @@ describe('Interactions', () => {
   describe('Example 1', () => {
 
     it('demonstrates simple contract interaction', async () => {
+      // Deploy SecretVault
       const SecretVault = await ethers.getContractFactory('SecretVault')
       let secretVault = await SecretVault.deploy('Secret')
 
+      // Deploy Interactions1
       const Contract = await ethers.getContractFactory('Interactions1')
       let contract = await Contract.deploy(secretVault.address)
 
+      // Get accounts
+
+      // Default secret state set to 'Secret'
       expect(await contract.getSecret()).to.equal('Secret')
+
+      // Set secret state to 'New Secret'
       await contract.setSecret('New Secret')
+      // Check if secret state is 'New Secret'
       expect(await contract.getSecret()).to.equal('New Secret')
 
     })
   })
 
-  describe('Example 2', () => {
+  // describe('Example 2', () => {
 
-    it('demonstrates simple contract interaction', async () => {
-      const Token = await ethers.getContractFactory('Token')
-      let token = await Token.deploy('My Token', 'MTK', tokens(1000000))
+  //   it('demonstrates simple contract interaction', async () => {
+  //     const Token = await ethers.getContractFactory('Token')
+  //     let token = await Token.deploy('My Token', 'MTK', tokens(1000000))
 
-      const Contract = await ethers.getContractFactory('Interactions2')
-      let contract = await Contract.deploy()
+  //     const Contract = await ethers.getContractFactory('Interactions2')
+  //     let contract = await Contract.deploy()
 
-      accounts = await ethers.getSigners()
-      owner = accounts[0]
+  //     accounts = await ethers.getSigners()
+  //     owner = accounts[0]
 
-      // Approve tokens
-      await token.approve(contract.address, tokens(1000000))
-      // Deposit
-      await contract.deposit(token.address, tokens(1000000))
+  //     // Approve tokens
+  //     await token.approve(contract.address, tokens(1000000))
+  //     // Deposit
+  //     await contract.deposit(token.address, tokens(1000000))
 
-      expect(await token.balanceOf(contract.address)).to.equal(tokens(1000000))
-    })
-  })
+  //     expect(await token.balanceOf(contract.address)).to.equal(tokens(1000000))
+  //   })
+  // })
 
 })
